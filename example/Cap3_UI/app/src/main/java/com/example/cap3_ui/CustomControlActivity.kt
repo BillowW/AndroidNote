b538beab2a533081b170cae522e60f485cd73cf2
Lihua <oncwnubrcihrf9o94dojz-6wr5ja@git.weixin.qq.com>
2021-06-02 16:22:55 +0800

    update

---
+++ example/Cap3_UI/app/src/main/java/com/example/cap3_ui/CustomControlActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cap3_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomControlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_control)
+    }
+}
