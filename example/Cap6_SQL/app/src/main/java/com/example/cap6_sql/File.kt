0c59de0bd16cba38115ed9da546208ba9be89de2
Lihua <oncwnuBrciHRf9o94dojz-6wr5jA@git.weixin.qq.com>
2021-06-09 13:30:33 +0800

    finish Broadcast

---
+++ example/Cap6_SQL/app/src/main/java/com/example/cap6_sql/File.kt
@@ -0,0 +1,11 @@
+package com.example.cap6_sql
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class File : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file)
+    }
+}
